472accefad8b0cefe0f2754a9f090a1ba8c5bc44
OleguerGabernet <oleguer.gabernet@gmail.com>
2021-04-05 11:02:56 +0200

    color

--- laberinth.js
+++ laberinth.js
@@ -1,25 +1,43 @@
 
-var div = 25;
+var div = 20;
+var strk = 15;
 var dim = 0;
 var a = 20;
 var fr;
 var r = 0;
 
+var color1;
+var color2;
+var color3;
+
+
 function setup() {
   createCanvas(1080, 1080);
   fr = createP(' ');
-  background(200);
   dim = width/div;
+  
+  color1 = color('#411E22');
+  color2 = color("#58A6A6");
+  color3 = color("#EFA355");
+  background(color1);
 }
 
 function draw() {
   stroke(50);
-  strokeWeight(1);
+  strokeWeight(strk);
+  strokeCap(PROJECT)
   for (var i=0; i < div; i++){
     for (var j=0; j < div; j++){
       
       r = int(random(2));
 
+      if ( r == 1){
+        stroke(color2);
+      }
+      else{
+        stroke(color3);
+      }
+      
       line((i+r)*dim, (j)*dim, (i+1-r)*dim, (j+1)*dim);
       fr.html(i);
     }
@@ -29,6 +47,9 @@ function draw() {
   // line((0+r)*width, 0, (1-r)*width, height);
  
   // a = int(random(2));
-  fr.html(r);
+  //fr.html(color1);
+  
+  //saveCanvas('laberinthJPG_canvas', 'JPG');
+  
   noLoop();
 }
